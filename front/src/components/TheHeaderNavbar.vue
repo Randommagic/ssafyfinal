a2dd9e706563e48e71857d1d1d96b5f68413d5bd
april2nds <rlawnsgh8395@naver.com>
2022-11-19 23:53:37 +0900

    [ADD] 메뉴바 수정 중

--- front/src/components/TheHeaderNavbar.vue
+++ front/src/components/TheHeaderNavbar.vue
@@ -1,71 +1,52 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar toggleable="lg" variant="light">
       <b-navbar-brand href="#">
         <router-link :to="{ name: 'main' }">
-          <b-img
-            :src="require('@/assets/ssafy_logo.png')"
-            id="logo"
-            class="d-inline-block align-top"
-            alt="logo"
-          ></b-img>
+          <b-img :src="require('@/assets/ssafy_logo.png')" id="logo" class="d-inline-block align-top" alt="logo">
+          </b-img>
         </router-link>
       </b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">
+          <b-nav-item href="#" variant="">
             <router-link :to="{ name: 'main' }" class="link">
-              <b-icon icon="house-door" animation="fade" font-scale="2"></b-icon>
-              메인
-            </router-link>
-            <router-link :to="{ name: 'insta' }" class="m-2 link">
-              <b-icon icon="instagram" animation="fade" font-scale="2"></b-icon>
-              인스타그램
+              홈
             </router-link>
             <router-link :to="{ name: 'board' }" class="m-2 link">
-              <b-icon icon="journal" animation="fade" font-scale="2"></b-icon>
-              게시판
+              공지사항
             </router-link>
             <router-link :to="{ name: 'house' }" class="m-2 link">
-              <b-icon icon="house-fill" animation="fade" font-scale="2"></b-icon>
-              아파트정보
+              지도
             </router-link>
-            <router-link :to="{ name: 'todo' }" class="link">
-              <b-icon icon="calendar-check" animation="fade" font-scale="2"></b-icon>
-              TodoList
+            <router-link :to="{ name: 'board' }" class="m-2 link">
+              자유게시판
             </router-link>
           </b-nav-item>
         </b-navbar-nav>
 
-        <!-- after login -->
-        <b-navbar-nav class="ml-auto" v-if="userInfo">
-          <b-nav-item class="align-self-center">
-            <b-avatar variant="primary" v-text="userInfo.userid.charAt(0).toUpperCase()"></b-avatar>
-            {{ userInfo.username }}({{ userInfo.userid }})님 환영합니다.
-          </b-nav-item>
-          <b-nav-item class="align-self-center">
-            <router-link :to="{ name: 'mypage' }" class="link align-self-center">내정보보기</router-link>
-          </b-nav-item>
-          <b-nav-item class="align-self-center link" @click.prevent="onClickLogout">로그아웃</b-nav-item>
+
+
+        <!-- 관리자 -->
+        <b-navbar-nav class="ml-auto" v-if="userInfo">        
+          <b-nav-item-dropdown id="my-nav-dropdown" toggle-class="nav-link-custom" right>
+            <template #button-content>{{userInfo.username}} 님</template>
+            <b-dropdown-item v-if="userInfo.category == 1">
+              <router-link :to="{ name: 'mypage' }" class="link align-self-center">회원 목록</router-link>
+            </b-dropdown-item>
+            <b-dropdown-item>
+              <router-link :to="{ name: 'mypage' }" class="link align-self-center">내정보보기</router-link>
+            </b-dropdown-item>
+            <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item variant="danger" @click.prevent="onClickLogout">로그아웃</b-dropdown-item>
+          </b-nav-item-dropdown>
         </b-navbar-nav>
+        <!-- 일반 유저-->
         <!-- before login -->
         <b-navbar-nav class="ml-auto" v-else>
-          <b-nav-item-dropdown right>
-            <template #button-content>
-              <b-icon icon="people" font-scale="2"></b-icon>
-            </template>
-            <b-dropdown-item href="#">
-              <router-link :to="{ name: 'join' }" class="link">
-                <b-icon icon="person-circle"></b-icon> 회원가입
-              </router-link>
+            <b-dropdown-item>
+              <router-link :to="{ name: 'login' }" class="link">로그인</router-link>
             </b-dropdown-item>
-            <b-dropdown-item href="#">
-              <router-link :to="{ name: 'login' }" class="link"> <b-icon icon="key"></b-icon> 로그인 </router-link>
-            </b-dropdown-item>
-          </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
