df7b0620f69a73a311577d3237e18a6f332d54bb
april2nds <rlawnsgh8395@naver.com>
2022-11-23 17:18:11 +0900

    [UPDATE] 북마크 리스트 수정

--- front/src/components/user/UserMyPageBookmark.vue
+++ front/src/components/user/UserMyPageBookmark.vue
@@ -1,15 +1,22 @@
 <template>
-    <b-container class="bv-example-row">
-    <div class="bookmark">
-      <b-col cols="9" class="bg-light p-2 pl-5 text-left">
-        <span :class="{ completed: apt.aptCOde }" >{{ apt.aptCode }}</span>
-      </b-col>
-      <b-button-group>
-      <b-button variant="danger" @click="removeApt">지도</b-button>
-      <b-button variant="danger"  @click="removeApt">삭제</b-button>
-    </b-button-group>
+    <!-- <b-container class="bv-example-row">
+            <b-button-group fluid>
+            <b-col cols="9" class="bg-light p-2 pl-5 text-left">
+                <span :class="{ completed: apt.aptCOde }">{{ apt.aptCode }}</span>
+            </b-col>
+                <b-button variant="primary" @click="removeApt">지도</b-button>
+                <b-button variant="danger" @click="removeApt">삭제</b-button>
+            </b-button-group>
+    </b-container> -->
+    <div>
+        <b-input-group prepend="아파트 명" class="mt-3">
+            <b-form-input v-model="aptInfo.aptCode" readonly></b-form-input>
+            <b-input-group-append>
+                <b-button variant="primary" @click="removeApt">지도</b-button>
+                <b-button variant="danger" @click="removeApt">삭제</b-button>
+            </b-input-group-append>
+        </b-input-group>
     </div>
-  </b-container>
 </template>
 
 <script>
@@ -17,11 +24,19 @@ import { mapState } from "vuex";
 const memberStore = "memberStore";
 export default {
     name: "UserMyPageBookMark",
+    data() {
+            return {
+                aptInfo: null,
+            }
+        },
+    created() {
+        this.aptInfo = this.apt;
+    },
     computed: {
         ...mapState(memberStore, ["userInfo"]),
     },
     props: {
-        apt : Object,
+        apt: Object,
     },
     methods: {
         removeApt() {
@@ -34,14 +49,15 @@ export default {
 </script>
 
 <style>
-.bookmark{
-    display:flex;
+.bookmark {
+    display: contents;
     margin: 10px;
 }
-.bookmark > button {
-    width:100px;
-    margin-left:15px;
-    font-size: 0.5rem;
-}
 
+/* 
+.bookmark>button {
+    width: 100px;
+    margin-left: 15px;
+    font-size: 0.5rem;
+} */
 </style>
